f0cafcba5d3796baf963ff3aec448201a2a8ae57
Victor <victorv24@gmail.com>
2021-03-29 18:38:42 -0700

    Basic elements set up for user form and table view.

---
+++ script.js
@@ -0,0 +1,9 @@
+// TO-DO
+// Each expense should include:
+// - The date of expense
+// - A description of the expense
+// - The amount
+// - Where the purchases occurred (eg. McDonald’s)
+// User should be able to add a new expense
+// User should be able to delete expense
+// User should be able to see all of the expenses in an HTML table
