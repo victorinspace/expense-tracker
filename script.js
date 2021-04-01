176b55140aac3c7deb0daabe072d8aecbfc9afbb
Victor <victorv24@gmail.com>
2021-03-31 17:54:39 -0700

    Submitting for code review.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // TO-DO
+// ==========
 // Each expense should include:
 // - The date of expense
 // - A description of the expense
@@ -7,3 +8,20 @@
 // User should be able to add a new expense
 // User should be able to delete expense
 // User should be able to see all of the expenses in an HTML table
+
+const submitExpense = (e) => {
+  e.preventDefault;
+
+  // === grab the date ===
+  const expenseDateInput = document.getElementById("date").value;
+  console.log(expenseDateInput);
+
+  // === grab the amount ===
+  // === grab the item ===
+  // === grab the comment (if there) ===
+  // === append the added expense to the expense table ===
+};
+
+document
+  .getElementById("expense-form")
+  .addEventListener("submit", submitExpense);
